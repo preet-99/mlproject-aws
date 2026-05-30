93ae970772010dff787fca3933e10c23b2f691ad
Preet Vishwakarma <159623026+preet-99@users.noreply.github.com>
2026-05-30 22:42:03 +0530

    commit workflow

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\study.csv')
+            df = pd.read_csv(r'notebook\data\study.csv')
             logging.info("Read the dataset as dataframe")
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True) ## make artifact folder if it not made 
